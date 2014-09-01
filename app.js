a8d13e5438213008dc28c6f398e43b931bb7c150
Brad Orego <me+github@bradorego.com>
2014-09-01 11:23:18 -0500

    stupid heroku.....

--- app.js
+++ app.js
@@ -35,10 +35,11 @@ app.use(function (req, res, next) {
           req.body._id = user._id;
           next();
         } else {
-          var err = new Error('Invalid auth');
-          err.status = 401;
-          err.message = 'Invalid auth';
-          next(err);
+          // var err = new Error('Invalid auth');
+          // err.status = 401;
+          // err.message = 'Invalid auth';
+          // next(err);
+          next();
         }
       });
     } else {
